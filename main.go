229db080b742a8785c4affef5e69eb58e4503ecb
GalvinGao <me@galvingao.com>
2022-08-25 00:44:25 -0400

    feat: add hostname

--- main.go
+++ main.go
@@ -12,7 +12,11 @@ var (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, world! This is version " + Version + " built at " + BuildTime))
+		hostname, err := os.Hostname()
+		if err != nil {
+			hostname = "#UnknownHostname"
+		}
+		w.Write([]byte("Hello, world! This is version " + Version + " built at " + BuildTime + ", running on " + hostname))
 	})
 	http.ListenAndServe(os.Getenv("SCRATCHPAD_ADDRESS"), nil)
 }
