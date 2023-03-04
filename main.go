fe3b2290abccffe14fa8732df4b37b76ccf1b0f8
GalvinGao <me@galvingao.com>
2023-03-04 15:57:19 -0500

    feat: add prometheus for testing canary analysis

--- main.go
+++ main.go
@@ -3,20 +3,47 @@ package main
 import (
 	"net/http"
 	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
 	Version   = "Unknown"
 	BuildTime = "Unknown"
+
+	HTTPStatus = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "scratchpad_http_status",
+			Help: "HTTP status codes",
+		},
+		[]string{"code"},
+	)
 )
 
 func main() {
+	prometheus.MustRegister(HTTPStatus)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		hostname, err := os.Hostname()
 		if err != nil {
 			hostname = "#UnknownHostname"
 		}
-		w.Write([]byte("Hello, world! This is version " + Version + " built at " + BuildTime + ", running on " + hostname))
+
+		greetings := "Hello, world!"
+
+		if r.URL.Query().Get("mockerror") != "" {
+			w.WriteHeader(http.StatusInternalServerError)
+			greetings = "Mocked error!"
+			HTTPStatus.WithLabelValues("500").Inc()
+		} else {
+			w.WriteHeader(http.StatusOK)
+			HTTPStatus.WithLabelValues("200").Inc()
+		}
+
+		w.Write([]byte(greetings + " This is version " + Version + " built at " + BuildTime + ", running on " + hostname))
 	})
+
+	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(os.Getenv("SCRATCHPAD_ADDRESS"), nil)
 }
